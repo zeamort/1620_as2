f23b6c1299d1b4ec7ca75e9b4486e29bd2ff83a9
morteza <zeamort@gmail.com>
2022-04-08 14:49:11 -0700

    Changed remote repo URL to my (Morteza) own. Pushed before beginning any modifications. Then wrote JS event and function (createNewNote) that on click of the plus icon will create a textarea with a save and cancel button. This will also remove the icon.

--- src/app.js
+++ src/app.js
@@ -5,3 +5,59 @@ const notes = [
     id: 1 
   }
 ]
+
+/*
+const newNoteBtn = document.querySelector(".fa-circle-plus")
+const textArea = document.querySelector('.write-note-area')
+
+function createNoteTakingArea(evt) {
+  let noteTakingArea = '<input type="button" value="Clear" onclick="javascript:eraseText();"><textarea name="textBox" id="textBox" cols="200" rows="50" placeholder="Write your notes here..."></textarea>'
+  textArea.insertAdjacentHTML("beforebegin", noteTakingArea)
+};
+
+newNoteBtn.addEventListener('click', createNoteTakingArea)
+*/
+// const saveTextBtn = document.querySelector(".fa-circle-plus")
+
+// function newNote(noteBody) {
+//   const template = `
+//     <textarea name="textBox" id="textBox" cols="200" rows="50" placeholder="Write your notes here..."></textarea>
+//     <button>Save</button>
+//     <button>Cancel</button>`
+//   return template
+// }
+
+// function displayNote(note) {
+//   const noteDisplayArea = document.querySelector(".write-note-area")
+//   noteDisplayArea.innerHTML = ''
+//   noteDisplayArea.insertAdjacentHTML('afterbegin', note)
+// }
+
+// function getNote() {
+//   const textArea = document.querySelector('.write-note-area')
+//   const note = textArea.value
+//   return note
+// }
+
+// function assembleNote() {
+//   const noteText = getNote()
+//   const note = newNote(noteText)
+//   displayNote(note)
+// }
+
+// saveTextBtn.addEventListener('click', assembleNote)
+
+const newNoteBtn = document.querySelector(".icons")
+const noteArea = document.querySelector(".create-note-area")
+
+newNoteBtn.addEventListener('click', createNewNote)
+
+function createNewNote() {
+  const noteTemplate = `
+  <textarea name="newNote" id="newNote" cols="210" rows="60" placeholder="write here..."></textarea>
+  <button type="submit">Save</button>
+  <button type="reset">Cancel</button>
+  `
+  noteArea.innerHTML = ''
+  noteArea.insertAdjacentHTML("afterbegin", noteTemplate)
+}
