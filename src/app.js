decefac64b2735cb56062fc34154ec520a237277
morteza <zeamort@gmail.com>
2022-04-10 15:16:52 -0700

    Implemented the function addToSideNav that takes a title as input and formats it as a <li></li> before inserting it into the side nav ul.

--- src/app.js
+++ src/app.js
@@ -6,61 +6,21 @@ const notes = [
   }
 ]
 
-/*
-const newNoteBtn = document.querySelector(".fa-circle-plus")
-const textArea = document.querySelector('.write-note-area')
-
-function createNoteTakingArea(evt) {
-  let noteTakingArea = '<input type="button" value="Clear" onclick="javascript:eraseText();"><textarea name="textBox" id="textBox" cols="200" rows="50" placeholder="Write your notes here..."></textarea>'
-  textArea.insertAdjacentHTML("beforebegin", noteTakingArea)
-};
-
-newNoteBtn.addEventListener('click', createNoteTakingArea)
-*/
-// const saveTextBtn = document.querySelector(".fa-circle-plus")
-
-// function newNote(noteBody) {
-//   const template = `
-//     <textarea name="textBox" id="textBox" cols="200" rows="50" placeholder="Write your notes here..."></textarea>
-//     <button>Save</button>
-//     <button>Cancel</button>`
-//   return template
-// }
-
-// function displayNote(note) {
-//   const noteDisplayArea = document.querySelector(".write-note-area")
-//   noteDisplayArea.innerHTML = ''
-//   noteDisplayArea.insertAdjacentHTML('afterbegin', note)
-// }
-
-// function getNote() {
-//   const textArea = document.querySelector('.write-note-area')
-//   const note = textArea.value
-//   return note
-// }
-
-// function assembleNote() {
-//   const noteText = getNote()
-//   const note = newNote(noteText)
-//   displayNote(note)
-// }
-
-// saveTextBtn.addEventListener('click', assembleNote)
-
 const newNoteBtn = document.querySelector(".icons")
 const noteArea = document.querySelector(".write-note-area")
 let noteID = 1
 
-
+//This event listener for the new note icon will call the createNewNote function.
 newNoteBtn.addEventListener('click', createNewNote)
 
+//This function will create the note taking environment. It will also create event listeners for save and cancel buttons.
 function createNewNote() {
   const noteTemplate = `
   <textarea name="newNote" id="newNote" cols="210" rows="50" placeholder="write here..."></textarea>
   <button id="saveButton" type="submit">Save</button>
   <button id="cancelButton" type="reset">Cancel</button>
   `
-  
+
   newNoteBtn.innerHTML = ''
   noteArea.insertAdjacentHTML("afterbegin", noteTemplate)
 
@@ -72,14 +32,10 @@ function createNewNote() {
   saveBtn.addEventListener('click', (evt) => {
     let noteLines = document.getElementById("newNote").value.split("\n")
     let noteTitle = noteLines[0];
+    addToSideNav(noteTitle)
 
     let noteContent = noteLines[1];
-    // let index = 1
-    // while (index < noteLines.length) {
-    //   noteContent.push(noteLines[index])
-    //   index += 1
-    // }
-
+    
     notes.push({title: noteTitle, noteBody: noteContent, id: (noteID+1)})
     noteID = noteID + 1;
     removeNoteTemplate()
@@ -93,3 +49,12 @@ function removeNoteTemplate() {
   newNoteBtn.innerHTML = '<i class="fa-solid fa-circle-plus">'
 }
 
+//Add to side nav
+const sideNavList = document.querySelector('.notes-list')
+
+function addToSideNav(title) {
+  titleProperHTML = '<li>' + title + '</li>'
+  sideNavList.insertAdjacentHTML("beforeend", titleProperHTML)
+}
+
+addToSideNav(notes[0].title)
